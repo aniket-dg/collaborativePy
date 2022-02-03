b3b99df0a40f109ded8a7b017f920e7cbfb9c92c
Mandar Parab <jontyparab@gmail.com>
2022-02-03 14:16:00 +0530

    ui updates

--- competition/views.py
+++ competition/views.py
@@ -6,8 +6,12 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.core.paginator import Paginator
 from django.contrib.auth.forms import UserCreationForm
 from .models import Competion, UserSubmission
+
+from .serializers import ReadOnlyCompetitionModelSerializer
+
 import calendar
 import datetime
 import numpy as np
@@ -18,8 +22,36 @@ class CompetionList(ListView):
     model=Competion
     context_object_name = 'list'
     template_name = 'complist.html'
-       #return render(request,'complist.html',context)
-       
+
+    def get_queryset(self):
+        today = datetime.date.today()
+        qs = super(CompetionList, self).get_queryset()
+        qs = qs.filter(start__lte=today).order_by('-start')
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        qs = self.get_queryset()
+        accept_type = request.META.get('HTTP_ACCEPT', None)
+        if (accept_type == 'application/json'):
+            try:
+                comp_status = request.GET.get('comp_status')
+                current_count = int(request.GET.get('current_count'))
+                comps = None
+                if (comp_status and current_count >= 0):
+                    qs = qs.filter(status=comp_status)
+                    p = Paginator(qs, 4)
+                    competitions = p.get_page((current_count + 4) / 4)
+                    comps = ReadOnlyCompetitionModelSerializer(competitions, many=True).data
+                    if p.count <= current_count:
+                        return JsonResponse({'status': False}, status=200)
+                return JsonResponse({'data': comps, 'status': True}, status=200)
+            except Exception as e:
+                print(e)
+        else:
+            return super(CompetionList, self).get(self, request, *args, **kwargs)
+        
+
+
 class CompetionDetail(DetailView):
     model = Competion
     template_name ='compdetail.html'
@@ -31,14 +63,19 @@ class CompetionDetail(DetailView):
         user = self.request.user
         if user.is_authenticated:
             context['user_submission']  = UserSubmission.objects.filter(user=user, competition=competition).last()
-        context['submissions'] = UserSubmission.objects.filter(competition=competition).order_by('-score', 'submission_date')
+        all_valid_submissions = UserSubmission.objects.filter(
+                                                        competition=competition,
+                                                        submission_date__date__lte=competition.end
+                                                        ).order_by('-score', 'submission_date')
+        context['submissions'] = all_valid_submissions
         return context
+
 
 class ParticipateView(LoginRequiredMixin, View):
     def post(self, *args, **kwargs):
         competition_id = self.request.POST.get('pk', None)
         today = datetime.date.today()
-        competition = Competion.objects.filter(id=competition_id, end__gte=today).last()
+        competition = Competion.objects.filter(id=competition_id).last()
         redirect_url = self.request.META.get('HTTP_REFERER')
         user = self.request.user
         if competition:
@@ -50,6 +87,7 @@ class ParticipateView(LoginRequiredMixin, View):
             return redirect(redirect_url)
         else:
             return redirect('Competion:detail', kwargs={'pk': competition_id})
+
 
 class UserSubmissionView(LoginRequiredMixin, View):
     # def get(self, *args, **kwargs):
@@ -63,7 +101,7 @@ class UserSubmissionView(LoginRequiredMixin, View):
         """Handles user submissions for competitions"""
         competition_id = self.kwargs.get('pk', None)
         today = datetime.date.today()
-        competition = Competion.objects.filter(id=competition_id, end__gte=today).last()
+        competition = Competion.objects.filter(id=competition_id, start__lte=today).last()
         user = self.request.user
         user_file = self.request.FILES.get('user_file', None)
         has_submitted = UserSubmission.objects.filter(competition=competition, user=user).exists()
@@ -74,11 +112,14 @@ class UserSubmissionView(LoginRequiredMixin, View):
             messages.error(self.request, 'No file uploaded!')
             return redirect('Competion:detail', pk=competition_id)
         if not competition:
-            messages.error(self.request, 'End date for the competition has passed.')
+            messages.error(self.request, 'No competition found.')
             return redirect('Competion:detail', pk=competition_id)
         if user in competition.participants.all():
             admin_file = competition.admin_file
-            score = self._get_submission_score(user_file, admin_file)
+            score, is_valid = self._get_submission_score(user_file, admin_file)
+            if not is_valid:
+                messages.error(self.request, 'Please check the demo file above and submit with proper column name.')
+                return redirect('Competion:detail', pk=competition_id)
             if (user_file and admin_file and competition and score != None):
                 user_submission = UserSubmission.objects.create(user=user, competition=competition,\
                                                             user_file=user_file,submission_date=datetime.datetime.now(),\
@@ -106,11 +147,12 @@ class UserSubmissionView(LoginRequiredMixin, View):
                 print(final_result)
                 correct_percent = 100 * df_comparison_column.sum() / len(admin_df.index)
                 percent = correct_percent.tolist().pop()  # Convert to python native type
-                return percent
+                return percent, True
             except Exception as e:
-                return 0
+                print('Submission NOT proper',e)
+                return 0, False
         else:
-            return 0
+            return 0, False
     
             
 
