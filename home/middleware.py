12db87e10299847c28471c9b92f909c272c9fff2
Aniket Gavali <aniket.dg25@gmail.com>
2022-10-08 19:41:04 +0530

    middleware updates

--- home/middleware.py
+++ home/middleware.py
@@ -7,9 +7,15 @@ from django.utils.deprecation import MiddlewareMixin
 class MyMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
+        unauthorized_urls = [
+            '/analytics/','/competition/', '/post/'
+        ]
         if request.user.is_authenticated and request.user.is_company_user():
-            if '/chat/' in request.path or '/media/' in request.path or '/order/' in request.path or 'user/' in request.path or 'jupyter' in request.path:
-                return None
-            elif '/company/' not in request.path:
-                return redirect('company:home')
+            # if '/chat/' in request.path or '/media/' in request.path or '/order/' in request.path or 'user/' in request.path or 'jupyter' in request.path:
+            #     return None
+            # elif '/company/' not in request.path:
+            #     return redirect('company:home')
+            for item in unauthorized_urls:
+                if item in request.path:
+                    return redirect('company:home')
         return None
