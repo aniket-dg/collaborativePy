1df444879f074c336d43630d27a92d3638a536a4
Aniket Gavali <aniket.gavali@noovosoft.com>
2023-03-27 17:59:29 +0530

    Change url for redirecting to jupyter

--- users/views.py
+++ users/views.py
@@ -102,7 +102,7 @@ class SaveSessionForNotebook(LoginRequiredMixin, View):
                 user.is_group_share = False
                 user.peer_id = request_user.id
                 user.save()
-                return redirect("https://jupyter.collaborativepy.com/jupyter/")
+                return redirect("http://13.127.56.121/jupyter/")
         if group_share and group_id:
             user.is_group_share = True
             user.is_peer_share = False
